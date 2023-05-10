ca3a665d24b169f0e8742841aafdd2445ee2aee8
AlexanderM2004 <amquizhpe@espe.edu.ec>
2023-05-10 12:50:04 -0500

    Practica 10/05/23

---
+++ POO_Practica1(Variables)_AlexanderQ/src/poo_practica1_alexanderq/POO_Practica1_AlexanderQ.java
@@ -0,0 +1,20 @@
+package poo_practica1_alexanderq;
+public class POO_Practica1_AlexanderQ {
+    public static void main(String[] args) {
+       byte bytAM = 5;
+       short shoAM = 200;
+       int inAM = 100;
+       long lonAM = 10000;
+       char chaAM = 'a';
+       double douAM = 4.5;
+       float floAM = 3.1f;
+       String strAM = "Hola mundo";
+       boolean boolAM = 10 > 5;
+       
+       System.out.println (chaAM);
+       System.out.println (bytAM);
+       System.out.println (boolAM);
+             
+    }
+    
+}
---
+++ POO_Practica5(Ramdom)_AlexanderQ/src/poo_practica5/ramdom/_alexanderq/POO_Practica5Ramdom_AlexanderQ.java
@@ -0,0 +1,13 @@
+//Generar numero ramdom
+package poo_practica5.ramdom._alexanderq;
+public class POO_Practica5Ramdom_AlexanderQ {
+    public static void main(String[] args) {
+        int x=0;
+        x =(int)(Math.random()*100);
+        boolean resultado = x >=10 && x<=60;
+        System.out.println(Math.random());
+        //System.out.println(x);
+        System.out.println(resultado);
+    }
+    
+}
