e05f529f27686b0bf5efc0baeee6d70c8c3bae94
AlexanderM2004 <132873838+AlexanderM2004@users.noreply.github.com>
2023-05-13 20:59:14 -0500

    Practica 10/05/23

---
+++ POO_Practica2(Operadores)_AlexanderQ/src/poo_practica2/operadores/_alexanderq/POO_Practica2Operadores_AlexanderQ.java
@@ -0,0 +1,38 @@
+//Operadores
+package poo_practica2.operadores._alexanderq;
+public class POO_Practica2Operadores_AlexanderQ {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 4;
+        int suma,resta,multi,divi;
+        System.out.println("--------------------------------------------");
+        System.out.print("Primer numero: ");
+        System.out.println(a);
+        System.out.print("Segundo numero: ");
+        System.out.println(b);
+        System.out.println("--------------------------------------------");
+        System.out.print("La suma de los numeros es: ");
+        suma=a+b;
+        System.out.println(suma);
+        System.out.println("--------------------------------------------");
+        System.out.print("La resta de los numeros es: ");
+        resta=a-b;
+        System.out.println(resta);
+        System.out.println("--------------------------------------------");
+        System.out.print("La mutiplicacion de los numeros es: ");
+        multi=a*b;
+        System.out.println(multi);
+        System.out.println("--------------------------------------------");
+        System.out.print("La divicion de los numeros es: ");
+        divi=a/b;
+        System.out.println(divi);
+        System.out.println("--------------------------------------------");
+        System.out.print("Primer numero aumentado 1: ");
+        a++;
+        System.out.println(a);
+        System.out.print("Segundo numero aumentado 1: ");
+        b++;
+        System.out.println(b);
+    }
+    
+}
---
+++ POO_Practica4(OLogicos)_AlexanderQ/src/poo_practica4/ologicos/_alexanderq/POO_Practica4OLogicos_AlexanderQ.java
@@ -0,0 +1,11 @@
+//Operadores Logicos
+package poo_practica4.ologicos._alexanderq;
+public class POO_Practica4OLogicos_AlexanderQ {
+    public static void main(String[] args) {
+        int val1 = 7, val2 = 2;
+        System.out.println((val1 == val2)&&(val1 <= val2)); //Operqador logico AND
+        System.out.println((val1 >= val2)||(val1 != val2)); //Operador logico OR
+        System.out.println(val1 != val2); //Operador logico NOT
+    }
+    
+}
